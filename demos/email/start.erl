d04d4ef3922309af6c7af0e66b294ee1efbd7166
mxolson <mxolson@godaddy.com>
2015-06-26 09:41:21 -0700

    fix arg to running

--- demos/email/start.erl
+++ demos/email/start.erl
@@ -4,8 +4,8 @@
 start(Browser) ->
 	Text = io_lib:format("~p", [Browser]),
 	PIDName = list_to_atom(lists:nth(1, Text)), 
-	ChatClient = chat_client:start(PIDName),
-	running(Browser, ChatClient).
+	chat_client:start(PIDName),
+	running(Browser, PIDName).
 
 running(Browser, ChatClient) ->
 	io:format("~p~n", [Browser]),
